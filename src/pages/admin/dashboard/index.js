01f61622090aa3dc14efb17bbfe70a782cf28d95
erikcc02 <designer231@gmail.com>
2021-11-02 16:57:33 -0300

    feat: create dashboard page

--- src/pages/admin/dashboard/index.js
+++ src/pages/admin/dashboard/index.js
@@ -1,16 +1,58 @@
 import Header from '../../../components/header'
 import Footer from '../../../components/footer'
 import Wrapper from '../../../components/wrapper'
+import './dashboard.scss';
+import { Container, Row, Col, Card } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBriefcase, faUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faBriefcase, faUser);
 
 function Dashboard() {
     return (
         <>
             <Header />
             <Wrapper>
-                <h1>Dashboard</h1>
-                <p>
-                    <a href="/">Voltar</a>
-                </p>
+                <Container>
+                    <Row>
+                        <Col md={12}>
+                            <h2>Dashboard</h2>
+                        </Col>
+                    </Row>
+                    <br />
+                    <Row>
+                        <Col md={4}>
+                            <Card className="panel panel-default painel-dashboard-fox">
+                                <a href="#">
+                                    <Card.Body className="panel-body">
+                                        <div class="icon">
+                                            <FontAwesomeIcon icon="briefcase" />
+                                        </div>
+                                        <div class="text">
+                                            Projetos
+                                        </div>
+                                    </Card.Body>
+                                </a>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card className="panel panel-default painel-dashboard-fox">
+                                <a href="#">
+                                    <Card.Body className="panel-body">
+                                        <div class="icon">
+                                            <FontAwesomeIcon icon="user" />
+                                        </div>
+                                        <div class="text">
+                                            Gerenciamento de Usuários
+                                        </div>
+                                    </Card.Body>
+                                </a>
+                            </Card>
+                        </Col>
+                        <Col md={4}></Col>
+                    </Row>
+                </Container>
             </Wrapper>
             <Footer />
         </>
