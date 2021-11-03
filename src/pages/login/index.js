a8930405ecc2fd90ecb4514c2d6186994e1773f6
erikcc02 <designer231@gmail.com>
2021-11-03 10:32:22 -0300

    chore: create login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,18 +1,40 @@
-import Header from '../../components/header'
-import Footer from '../../components/footer'
+import './login.scss'
+import { Form, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCheckSquare} from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from 'react';
 
 function Login() {
+
+    library.add(faCheckSquare);
+    const [spinner, setSpinner] = useState(true);
+
+    useEffect(() => {
+        setSpinner(false);
+    }, []);
+
     return (
-        <>
-            <Header/>
-            <div>
-                <h1>Login</h1>
-                <p>
-                    <a href="/">Voltar</a>
-                </p>
-            </div>
-            <Footer/>
-        </>
+        <div class="login">
+            { spinner && <div id="loader"></div> }
+
+            <Form>
+                <div id="login" class="position-absolute top-50 start-50 translate-middle">
+                    <div class ="logo">
+                        <a href="#">
+                            <FontAwesomeIcon icon="check-square" /> Status Projetos
+                        </a>
+                    </div>
+
+                    <div class ="wrapper-login">
+                        <Form.Control type="email" placeholder="E-mail" className="user" autoComplete="off" />
+                        <Form.Control type="password" placeholder="Senha" className="password" autoComplete="off" />
+                    </div>
+
+                    <Button type ="submit" className="login-button">Entrar</Button>
+                </div>
+            </Form>
+        </div>
     )
 }
 
