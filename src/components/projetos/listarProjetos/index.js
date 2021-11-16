7fea5a8bc902e53f7dfef5eb029ed88dfca0247d
erikcc02 <designer231@gmail.com>
2021-11-16 20:51:29 -0300

    chore: filter projects

--- src/components/projetos/listarProjetos/index.js
+++ src/components/projetos/listarProjetos/index.js
@@ -2,40 +2,115 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 import Moment from 'react-moment';
+import { Nav, Spinner } from 'react-bootstrap';
+import api from '../../../api';
+import { useState, useEffect } from 'react';
 
 library.add(faLongArrowAltRight);
 
 function ListarProjetos(props) {
-    return (
-        <div class="list-of-projects">
-            <h3>{props.busca}</h3>
 
-            {
-                props.projetos.map((projeto, index) => {
-                    return (
-                        <div class="box-project">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="card-fox card-project">
-                                        <div class="info">
-                                            <p>Informações</p>
-                                        </div>
-                                        <div class="content">
-                                            <h3>{projeto.nome}</h3>
-                                            <p><b>Cliente:</b> {projeto.cliente}</p>
-                                            <p><b>Previsão de entrega:</b>  <Moment locale="pt-br" format="DD-MM-YYYY">{projeto.data_previsao_entrega}</Moment></p>
-                                            <p class="text-end">
-                                                <a class="btn btn-fox-dynamic" href={`/admin/projetos/timeline/${projeto.id}`}><FontAwesomeIcon icon="long-arrow-alt-right" /> Ver Projeto</a>
-                                            </p>
+    const [spinner, setSpinner] = useState(true);
+    const [projetos, setProjetos] = useState([]);
+    const [countProjetos, setCountProjetos] = useState(0);
+
+    const [habilitaEmAndamento, setHabilitaEmAndamento] = useState(true);
+    const [habilitaAguardandoCliente, setHabilitaAguardandoCliente] = useState(false);
+    const [habilitaConcluido, setHabilitaConcluido] = useState(false);
+
+    async function CarregarProjetos(status) {
+        await api.get('v1/api/projects/by-status/' + status)
+            .then(response => {
+                setProjetos(response.data);
+                setCountProjetos(projetos.length);
+                setSpinner(false);
+            })
+            .catch(error => {
+                setSpinner(false);
+                console.error(error);
+            });
+    }
+
+    function SelecionaStatusProjeto(status) {
+
+        setSpinner(true);
+
+        setHabilitaEmAndamento(false);
+        setHabilitaAguardandoCliente(false);
+        setHabilitaConcluido(false);
+
+        switch (status) {
+            case 'em-andamento':
+                setHabilitaEmAndamento(true);
+                CarregarProjetos('EM_ANDAMENTO');
+                break;
+            case 'aguardando-cliente':
+                setHabilitaAguardandoCliente(true);
+                CarregarProjetos('ANALISANDO_INFORMACOES');
+                break;
+            case 'concluido':
+                setHabilitaConcluido(true);
+                CarregarProjetos('CONCLUIDO');
+                break;
+        }
+    }
+
+    useEffect(() => {
+        setSpinner(true);
+        CarregarProjetos('EM_ANDAMENTO');
+    }, []);
+
+    return (
+        <>
+            <div className="container-spinner" hidden={!spinner}>
+                <Spinner
+                    id="custom-spinner"
+                    style={{ 'position': 'absolute', 'top': '50%', 'left': '50%' }}
+                    animation="border"
+                    role="status" />
+            </div>
+            <div class="nav-pills-fox-container">
+                <Nav defaultActiveKey="#1" variant="tabs" className="nav-pills-fox">
+                    <Nav.Item>
+                        <Nav.Link href="#1" onClick={() => SelecionaStatusProjeto('em-andamento')}>Em andamento {habilitaEmAndamento && `(` + countProjetos + `)`}</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#2" onClick={() => SelecionaStatusProjeto('aguardando-cliente')}>Aguardando o cliente {habilitaAguardandoCliente && `(` + countProjetos + `)`}</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#3" onClick={() => SelecionaStatusProjeto('concluido')}>Finalizados {habilitaConcluido && `(` + countProjetos + `)`}</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <hr />
+            </div>
+            <div class="list-of-projects">
+                {
+                    projetos.map((projeto, index) => {
+                        return (
+                            <div class="box-project">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="card-fox card-project">
+                                            <div class="info">
+                                                <p>Informações</p>
+                                            </div>
+                                            <div class="content">
+                                                <h3>{projeto.nome}</h3>
+                                                <p><b>Cliente:</b> {projeto.cliente}</p>
+                                                <p><b>Previsão de entrega:</b>  <Moment locale="pt-br" format="DD-MM-YYYY">{projeto.data_previsao_entrega}</Moment></p>
+                                                <p class="text-end">
+                                                    <a class="btn btn-fox-dynamic" href={`/admin/projetos/timeline/${projeto.id}`}><FontAwesomeIcon icon="long-arrow-alt-right" /> Ver Projeto</a>
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
+                        )
+                    })
+                }
+            </div>
+        </>
     );
 }
 
--- src/pages/admin/projetos/index.js
+++ src/pages/admin/projetos/index.js
@@ -3,48 +3,16 @@ import Footer from '../../../components/footer';
 import Wrapper from '../../../components/wrapper';
 import ListarProjetos from '../../../components/projetos/listarProjetos';
 import './projetos.scss';
-import { Container, Row, Col, Nav, Spinner } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import api from '../../../api';
-import { useState, useEffect } from 'react';
 
 library.add(faPlus);
 
 function Projetos() {
-    const [spinner, setSpinner] = useState(true);
-    let [projetos, setProjetos] = useState([]);
-
-    useEffect(() => {
-        setSpinner(true);
-
-        async function CarregarProjetos() {
-            await api.get('v1/api/projects/by-user')
-                .then(response => {
-                    setProjetos(response.data);
-                    setSpinner(false);
-                })
-                .catch(error => {
-                    setSpinner(false);
-                    console.error(error);
-                });
-        }
-
-        CarregarProjetos();
-    }, []);
-
-
     return (
         <>
-            <div className="container-spinner" hidden={!spinner}>
-                <Spinner
-                    id="custom-spinner"
-                    style={{ 'position': 'absolute', 'top': '50%', 'left': '50%' }}
-                    animation="border"
-                    role="status" />
-            </div>
-
             <Header />
             <Wrapper>
                 <Container>
@@ -61,22 +29,7 @@ function Projetos() {
                             </div>
                         </Col>
                     </Row>
-                    <div class="nav-pills-fox-container">
-                        <Nav defaultActiveKey="#1" variant="tabs" className="nav-pills-fox">
-                            <Nav.Item>
-                                <Nav.Link href="#1">Em andamento (0)</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#2">Aguardando o cliente</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#3">Finalizados</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        <hr />
-                    </div>
-
-                    <ListarProjetos busca="andamento" projetos={projetos} />
+                    <ListarProjetos />
                 </Container>
             </Wrapper>
             <Footer />
